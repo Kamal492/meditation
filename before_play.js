33810df35da5279a37de249796781416afc92d70
Kamal492 <58765013+Kamal492@users.noreply.github.com>
2021-07-23 21:59:28 +0530

    Update before_play.js

--- before_play.js
+++ before_play.js
@@ -1,4 +1,4 @@
-function openPlayer(event) {
+/*function openPlayer(event) {
 //  sessionStorage.setItem("med_id", event.target.id);
 //  meditation_id = sessionStorage.getItem("med_id");
 //  console.log(meditation_id);
@@ -9,6 +9,19 @@ function openPlayer(event) {
  //console.log(name);
  //console.log(id);
  url = "play.html?name="+name+"&id="+id;
- document.location.href = url;
+ //document.location.href = url;
+ console.log(url);
+ //window.open(url, "_self");
  //console.log(url);
+}*/
+var ele = document.getElementsByClassName('cards-div');
+for(i=0;i<ele.length;i++){
+  ele[i].addEventListener("click", function(event){
+    var music_id = event.currentTarget.id;
+    var name = music_id.split('-')[0];
+    var id = music_id.split('-')[1];
+    url = "play.html?name="+name+"&id="+id;
+    //console.log(url);
+    document.location.href = url;
+    });
 }
