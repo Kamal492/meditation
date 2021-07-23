38a5c1c4738a6a513f843e504c37f6eefb5a354e
Kamal492 <58765013+Kamal492@users.noreply.github.com>
2021-07-23 20:35:01 +0530

    Update before_play.js

--- before_play.js
+++ before_play.js
@@ -1,6 +1,14 @@
 function openPlayer(event) {
-  sessionStorage.setItem("med_id", event.target.id);
-  meditation_id = sessionStorage.getItem("med_id");
-  console.log(meditation_id);
-   window.open("play.html","_self")
+//  sessionStorage.setItem("med_id", event.target.id);
+//  meditation_id = sessionStorage.getItem("med_id");
+//  console.log(meditation_id);
+//   window.open("play.html","_self")
+ var music_id = event.target.id;
+ var name = music_id.split('-')[0];
+ var id = music_id.split('-')[1];
+ //console.log(name);
+ //console.log(id);
+ url = "play.html?name="+name+"&id="+id;
+ document.location.href = url;
+ //console.log(url);
 }
